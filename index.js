180bf2ab5bc0d4ce0dfcd03fc479b135744a6be5
WaqarAhmadSoftEng <waqarahmad.softeng@gmail.com>
2025-09-27 14:16:30 +0500

    math module

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Hello Sir');
+
+const math = require("./math");
+// for addition
+console.log(math.add(2, 3));
+// for subtraction
+console.log(math.sub(2, 3));
+// for multiplication
+console.log(math.mul(2, 3));
+// for divission
+console.log(math.dev(2, 3));
